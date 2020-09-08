1c9c5f5aa2e90428409464596c032a5371366c0b
DedeApenas <Andrelromao@hotmail.com>
2020-09-08 15:34:12 -0300

    feat: Exercicio05

---
+++ Exercicio05.py
@@ -0,0 +1,8 @@
+primeiraLista = ["Azul", "Vermelho", "Preto"]
+for i in primeiraLista:
+ print("Item da lista: ",i)
+
+if "Azul" in primeiraLista:
+ print("Sim,está na lista")
+else:
+ print("Não está na lista")
