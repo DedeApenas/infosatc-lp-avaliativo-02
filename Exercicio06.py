228c665854f8967d34ee1527b6f817c22579f3e5
DedeApenas <Andrelromao@hotmail.com>
2020-09-08 15:40:52 -0300

    feat: Exercicio06

---
+++ Exercicio06.py
@@ -0,0 +1,8 @@
+filmesLista = ["Projeto X","Superbad","Interestelar"]
+print(filmesLista)
+jogosLista = ["Bioshock Infinite","Dishonored","Life is Strange"]
+print(jogosLista)
+livrosLista = ["Assassins Creed","O corvo","The Amityville Horror Jay Anson"]
+print(livrosLista)
+esportesLista = ["Futebol","Futebol Americano","Basquete"]
+print(esportesLista)
