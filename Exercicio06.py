9507a8ff00544e3127bdef565d317a0f8a394848
DedeApenas <Andrelromao@hotmail.com>
2020-09-08 15:52:19 -0300

    fix: Exercicio06

--- Exercicio06.py
+++ Exercicio06.py
@@ -2,7 +2,12 @@ filmesLista = ["Projeto X","Superbad","Interestelar"]
 print(filmesLista)
 jogosLista = ["Bioshock Infinite","Dishonored","Life is Strange"]
 print(jogosLista)
-livrosLista = ["Assassins Creed","O corvo","The Amityville Horror Jay Anson"]
+livrosLista = ["Assassins Creed Renegado","O corvo","The Amityville Horror Jay Anson"]
 print(livrosLista)
 esportesLista = ["Futebol","Futebol Americano","Basquete"]
 print(esportesLista)
+
+minhaLista = filmesLista + jogosLista + livrosLista + esportesLista
+print("Minha lista de passa tempo: ",minhaLista)
+
+print("O unico livro que li da lista livros foi:",livrosLista[0])
